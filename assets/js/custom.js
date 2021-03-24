c17047d829e4d6b958a2646b3956630789b2dace
RasedulRasel21 <frasel271@gmail.com>
2021-03-24 19:22:06 +0000

    js added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,58 +15,37 @@
 
   AOS.init();
 
-  //counter
-  $(document).ready(function($) {
-    //Check if an element was in a screen
-    function isScrolledIntoView(elem){
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
-        return ((elemBottom <= docViewBottom));
-    }
-    //Count up code
-    function countUp() {
-        $('.counter').each(function() {
-          var $this = $(this), // <- Don't touch this variable. It's pure magic.
-              countTo = $this.attr('data-count');
-              ended = $this.attr('ended');
-
-        if ( ended != "true" && isScrolledIntoView($this) ) {
-            $({ countNum: $this.text()}).animate({
-            countNum: countTo
-          },
-          {
-            duration: 2500, //duration of counting
-            easing: 'swing',
-            step: function() {
-              $this.text(Math.floor(this.countNum));
-            },
-            complete: function() {
-              $this.text(this.countNum);
-            }
-          });
-        $this.attr('ended', 'true');
-        }
-        });
-    }
-    //Start animation on page-load
-    if ( isScrolledIntoView(".counter") ) {
-        countUp();
-    }
-    //Start animation on screen
-    $(document).scroll(function() {
-        if ( isScrolledIntoView(".counter") ) {
-            countUp();
-        }
-    });
-
-
-});
 
 AOS.init({
   disable: function() {
     var maxWidth = 991;
     return window.innerWidth < maxWidth;
   }
+});
+
+
+// carousel
+
+
+$(document).ready(function(){
+  $('.carousel').slick({
+  slidesToShow: 1,
+  dots:false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+  ],
+  prevArrow: '<span class="prev-arrow"><img src="./assets/images/left.png" alt=""></span>',
+  nextArrow: '<span class="next-arrow"><img src="./assets/images/right.png" alt=""></span>',
+  });
 });
